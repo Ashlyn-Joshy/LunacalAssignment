26dafa920efb8136390118d5da841381fc9a9fd8
Ashlyn Joshy <ashlyntherese@gmail.com>
2024-09-02 22:05:12 +0530

    tailwind css added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,11 @@
+import "./index.css";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1 className="font-bold text-3xl">Hello</h1>
+    </div>
+  );
 }
 
 export default App;
